5344618e7b4dad0e2dc4f1338113c95f8f043e27
Bakkes <chris.m@live.nl>
2017-06-21 14:12:52 +0200

    Added json saving

--- VideoPlugin/VideoPlugin.cpp
+++ VideoPlugin/VideoPlugin.cpp
@@ -1,20 +1,71 @@
 #include "VideoPlugin.h"
 #include <map>
 #include <fstream>
+#include <cereal/types/unordered_map.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+
 BAKKESMOD_PLUGIN(VideoPlugin, "Video plugin", "0.2", 0)
 
 GameWrapper* gw = NULL;
 ConsoleWrapper* cons = NULL;
 
+static int current_id = 0;
 
-struct CamSave {
-	float timeStamp;
-	Vector location;
-	Rotator rotator;
-	float FOV;
+struct VectorSerializable {
+	Vector vector;
+
+	VectorSerializable(Vector v) : vector(v) {}
+	VectorSerializable() {}
+	template <class Archive>
+	void serialize(Archive & ar)
+	{
+		ar(CEREAL_NVP(vector.X), CEREAL_NVP(vector.Y), CEREAL_NVP(vector.Z));
+	}
 };
 
-std::map<float, CamSave> saves;
+struct RotatorSerializable {
+	Rotator rotator;
+
+	RotatorSerializable(Rotator r) : rotator(r) {}
+	RotatorSerializable() {}
+	template <class Archive>
+	void serialize(Archive & ar)
+	{
+		ar(CEREAL_NVP(rotator.Pitch), CEREAL_NVP(rotator.Yaw), CEREAL_NVP(rotator.Roll));
+	}
+};
+
+struct CamSave {
+	int id;
+	float timeStamp;
+	VectorSerializable location;
+	RotatorSerializable rotation;
+	float FOV;
+
+	CamSave() {}
+	template <class Archive>
+	void serialize(Archive & ar)
+	{
+		ar(CEREAL_NVP(id), CEREAL_NVP(timeStamp));
+		location.serialize(ar);
+		rotation.serialize(ar);
+		ar(CEREAL_NVP(FOV));
+	}
+};
+
+struct Path {
+	std::map<float, CamSave> saves;
+
+	template <class Archive>
+	void serialize(Archive & ar)
+	{
+		ar(CEREAL_NVP(saves));
+	}
+};
+
+Path currentPath;
 
 long long playback() {
 	ReplayWrapper sw = gw->GetGameEventAsReplay();
@@ -26,7 +77,7 @@ long long playback() {
 	CamSave nextSave;
 	prevSave.timeStamp = -1;
 	nextSave.timeStamp = -1;
-	for (auto it = saves.begin(); it != saves.end(); it++)
+	for (auto it = currentPath.saves.begin(); it != currentPath.saves.end(); it++)
 	{
 		if (it->first > replaySeconds && nextSave.timeStamp < 0)
 		{
@@ -44,38 +95,38 @@ long long playback() {
 		Rotator snapR = Rotator(frameDiff);
 		Vector snap = Vector(frameDiff);
 
-		//Vector newLoc = gw->GetCamera().linterp(prevSave.location, nextSave.location, timeElapsed, 1.0f); //; prevSave.location + ((nextSave.location - prevSave.location) * timeElapsed) / snap;
-		Vector newLoc = prevSave.location + ((nextSave.location - prevSave.location) * timeElapsed) / snap;
-		if (prevSave.rotator.Yaw < -32768 / 2 && nextSave.rotator.Yaw > 32768 / 2) {
-			nextSave.rotator.Yaw -= 32768 * 2;
+		//Vector newLoc = gw->GetCamera().linterp(prevSave.location.vector, nextSave.location.vector, timeElapsed, 1.0f); //; prevSave.location.vector + ((nextSave.location.vector - prevSave.location.vector) * timeElapsed) / snap;
+		Vector newLoc = prevSave.location.vector + ((nextSave.location.vector - prevSave.location.vector) * timeElapsed) / snap;
+		if (prevSave.rotation.rotator.Yaw < -32768 / 2 && nextSave.rotation.rotator.Yaw > 32768 / 2) {
+			nextSave.rotation.rotator.Yaw -= 32768 * 2;
 		}
-		if (prevSave.rotator.Yaw > 32768 / 2 && nextSave.rotator.Yaw < -32768 / 2) {
-			nextSave.rotator.Yaw += 32768 * 2;
-		}
-
-		if (prevSave.rotator.Roll < -32768 / 2 && nextSave.rotator.Roll > 32768 / 2) {
-			nextSave.rotator.Roll -= 32768 * 2;
-		}
-		if (prevSave.rotator.Roll > 32768 / 2 && nextSave.rotator.Roll < -32768 / 2) {
-			nextSave.rotator.Roll += 32768 * 2;
+		if (prevSave.rotation.rotator.Yaw > 32768 / 2 && nextSave.rotation.rotator.Yaw < -32768 / 2) {
+			nextSave.rotation.rotator.Yaw += 32768 * 2;
 		}
 
-		if (prevSave.rotator.Pitch < -16364 / 2 && nextSave.rotator.Pitch > 16364 / 2) {
-			nextSave.rotator.Pitch -= 16364 * 2;
+		if (prevSave.rotation.rotator.Roll < -32768 / 2 && nextSave.rotation.rotator.Roll > 32768 / 2) {
+			nextSave.rotation.rotator.Roll -= 32768 * 2;
 		}
-		if (prevSave.rotator.Pitch > 16364 / 2 && nextSave.rotator.Pitch < -16364 / 2) {
-			nextSave.rotator.Pitch += 16364 * 2;
+		if (prevSave.rotation.rotator.Roll > 32768 / 2 && nextSave.rotation.rotator.Roll < -32768 / 2) {
+			nextSave.rotation.rotator.Roll += 32768 * 2;
+		}
+
+		if (prevSave.rotation.rotator.Pitch < -16364 / 2 && nextSave.rotation.rotator.Pitch > 16364 / 2) {
+			nextSave.rotation.rotator.Pitch -= 16364 * 2;
+		}
+		if (prevSave.rotation.rotator.Pitch > 16364 / 2 && nextSave.rotation.rotator.Pitch < -16364 / 2) {
+			nextSave.rotation.rotator.Pitch += 16364 * 2;
 		}
 
 		Rotator newRot;
-		newRot.Pitch = prevSave.rotator.Pitch + ((nextSave.rotator.Pitch - prevSave.rotator.Pitch) * timeElapsed / frameDiff);
-		newRot.Yaw = prevSave.rotator.Yaw + ((nextSave.rotator.Yaw - prevSave.rotator.Yaw) * timeElapsed / frameDiff);
-		newRot.Roll = prevSave.rotator.Roll + ((nextSave.rotator.Roll - prevSave.rotator.Roll) * timeElapsed / frameDiff);
+		newRot.Pitch = prevSave.rotation.rotator.Pitch + ((nextSave.rotation.rotator.Pitch - prevSave.rotation.rotator.Pitch) * timeElapsed / frameDiff);
+		newRot.Yaw = prevSave.rotation.rotator.Yaw + ((nextSave.rotation.rotator.Yaw - prevSave.rotation.rotator.Yaw) * timeElapsed / frameDiff);
+		newRot.Roll = prevSave.rotation.rotator.Roll + ((nextSave.rotation.rotator.Roll - prevSave.rotation.rotator.Roll) * timeElapsed / frameDiff);
 
 
-		//Rotator newRot = prevSave.rotator + ((nextSave.rotator - prevSave.rotator) * timeElapsed) / snapR;
+		//Rotator newRot = prevSave.rotation.rotator + ((nextSave.rotation.rotator - prevSave.rotation.rotator) * timeElapsed) / snapR;
 		//newLoc = newLoc - (Vector(newRot.Pitch, newRot.Yaw, newRot.Roll) * 100.0);
-		//Vector newLoc = gw->VecInterp(prevSave.location, nextSave.location, timeElapsed, 1.0f);
+		//Vector newLoc = gw->VecInterp(prevSave.location.vector, nextSave.location.vector, timeElapsed, 1.0f);
 
 		//newRot.Yaw += 50;
 		POV p;
@@ -133,41 +184,41 @@ void run_playback() {
 //			Rotator snapR = Rotator(frameDiff);
 //			Vector snap = Vector(frameDiff);
 //
-//			Vector newLoc = prevSave.location + ((nextSave.location - prevSave.location) * timeElapsed) / snap;
+//			Vector newLoc = prevSave.location.vector + ((nextSave.location.vector - prevSave.location.vector) * timeElapsed) / snap;
 //			//-32768, -32768
-//			if (prevSave.rotator.Yaw < -32768 / 2 && nextSave.rotator.Yaw > 32768/2) {
-//				nextSave.rotator.Yaw -= 32768 * 2;
+//			if (prevSave.rotation.rotator.Yaw < -32768 / 2 && nextSave.rotation.rotator.Yaw > 32768/2) {
+//				nextSave.rotation.rotator.Yaw -= 32768 * 2;
 //			}
-//			if (prevSave.rotator.Yaw > 32768 / 2 && nextSave.rotator.Yaw < -32768 / 2) {
-//				nextSave.rotator.Yaw += 32768 * 2;
-//			}
-//
-//			if (prevSave.rotator.Roll < -32768 / 2 && nextSave.rotator.Roll > 32768 / 2) {
-//				nextSave.rotator.Roll -= 32768 * 2;
-//			}
-//			if (prevSave.rotator.Roll > 32768 / 2 && nextSave.rotator.Roll < -32768 / 2) {
-//				nextSave.rotator.Roll += 32768 * 2;
+//			if (prevSave.rotation.rotator.Yaw > 32768 / 2 && nextSave.rotation.rotator.Yaw < -32768 / 2) {
+//				nextSave.rotation.rotator.Yaw += 32768 * 2;
 //			}
 //
-//			if (prevSave.rotator.Pitch < -16364 / 2 && nextSave.rotator.Pitch > 16364 / 2) {
-//				nextSave.rotator.Pitch -= 16364 * 2;
+//			if (prevSave.rotation.rotator.Roll < -32768 / 2 && nextSave.rotation.rotator.Roll > 32768 / 2) {
+//				nextSave.rotation.rotator.Roll -= 32768 * 2;
 //			}
-//			if (prevSave.rotator.Pitch > 16364 / 2 && nextSave.rotator.Pitch < -16364 / 2) {
-//				nextSave.rotator.Pitch += 16364 * 2;
+//			if (prevSave.rotation.rotator.Roll > 32768 / 2 && nextSave.rotation.rotator.Roll < -32768 / 2) {
+//				nextSave.rotation.rotator.Roll += 32768 * 2;
 //			}
-//			prevSave.rotator.Pitch - nextSave.rotator.Pitch;
-//			//if(prevSave.rotator.Pitch
+//
+//			if (prevSave.rotation.rotator.Pitch < -16364 / 2 && nextSave.rotation.rotator.Pitch > 16364 / 2) {
+//				nextSave.rotation.rotator.Pitch -= 16364 * 2;
+//			}
+//			if (prevSave.rotation.rotator.Pitch > 16364 / 2 && nextSave.rotation.rotator.Pitch < -16364 / 2) {
+//				nextSave.rotation.rotator.Pitch += 16364 * 2;
+//			}
+//			prevSave.rotation.rotator.Pitch - nextSave.rotation.rotator.Pitch;
+//			//if(prevSave.rotation.rotator.Pitch
 //
 //			Rotator newRot;
-//			newRot.Pitch = prevSave.rotator.Pitch + ((nextSave.rotator.Pitch - prevSave.rotator.Pitch) * timeElapsed) / snapR.Pitch;
-//			newRot.Yaw = prevSave.rotator.Yaw + ((nextSave.rotator.Yaw - prevSave.rotator.Yaw) * timeElapsed) / snapR.Yaw;
-//			newRot.Roll = prevSave.rotator.Roll + ((nextSave.rotator.Roll - prevSave.rotator.Roll) * timeElapsed) / snapR.Roll;
+//			newRot.Pitch = prevSave.rotation.rotator.Pitch + ((nextSave.rotation.rotator.Pitch - prevSave.rotation.rotator.Pitch) * timeElapsed) / snapR.Pitch;
+//			newRot.Yaw = prevSave.rotation.rotator.Yaw + ((nextSave.rotation.rotator.Yaw - prevSave.rotation.rotator.Yaw) * timeElapsed) / snapR.Yaw;
+//			newRot.Roll = prevSave.rotation.rotator.Roll + ((nextSave.rotation.rotator.Roll - prevSave.rotation.rotator.Roll) * timeElapsed) / snapR.Roll;
 //			newRot = newRot + Rotator(0);
-//			//Rotator newRot = prevSave.rotator + ((nextSave.rotator - prevSave.rotator) * timeElapsed) / snapR;
+//			//Rotator newRot = prevSave.rotation.rotator + ((nextSave.rotation.rotator - prevSave.rotation.rotator) * timeElapsed) / snapR;
 //			//newLoc = newLoc - (Vector(newRot.Pitch, newRot.Yaw, newRot.Roll) * 100.0);
-//			//Vector newLoc = gw->VecInterp(prevSave.location, nextSave.location, timeElapsed, 1.0f);
+//			//Vector newLoc = gw->VecInterp(prevSave.location.vector, nextSave.location.vector, timeElapsed, 1.0f);
 //			POV p;
-//			p.location = newLoc + Vector(0);
+//			p.location.vector = newLoc + Vector(0);
 //			//p.rotation = newRot + Rotator(0);
 //			p.FOV = prevSave.FOV;
 //			gw->GetCamera().SetPOV(p); //, newRot
@@ -181,9 +232,9 @@ void run_playback() {
 
 void saveCam() {
 	std::ofstream out("out.fly", ios::out | ios::trunc | ios::binary);
-	int size = saves.size();
+	int size = currentPath.saves.size();
 	write_pod(out, size);
-	for (auto it = saves.begin(); it != saves.end(); it++)
+	for (auto it = currentPath.saves.begin(); it != currentPath.saves.end(); it++)
 	{
 		write_pod(out, it->second);
 	}
@@ -191,14 +242,15 @@ void saveCam() {
 
 void loadCam() {
 	std::ifstream in("out.fly", ios::binary);
-	saves.clear();
+	currentPath.saves.clear();
+	current_id = 0;
 	int size = 0;
 	read_pod(in, size);
 	for (unsigned int i = 0; i < size; i++)
 	{
 		CamSave save;
 		read_pod(in, save);
-		saves.insert(std::make_pair(save.timeStamp, save));
+		currentPath.saves.insert(std::make_pair(save.timeStamp, save));
 	}
 }
 
@@ -220,12 +272,19 @@ void videoPlugin_onCommand(std::vector<std::string> params)
 		//	return;
 		//} 
 		POV currentPov = flyCam.GetPOV();
-
-		save.location = currentPov.location;
-		save.rotator = currentPov.rotation;
+		save.id = current_id;
+		save.location = VectorSerializable(currentPov.location);
+		save.rotation = RotatorSerializable(currentPov.rotation);
 		save.FOV = currentPov.FOV;
-		saves.insert(std::make_pair(save.timeStamp, save));
+		currentPath.saves.insert(std::make_pair(save.timeStamp, save));
 		saveCam();
+		current_id++;
+
+		std::ofstream ofs("cam.json", std::ios::out | std::ios::trunc);  // Output file stream.
+		cereal::JSONOutputArchive  oarchive(ofs); // Choose binary format, writingdirection.
+		currentPath.serialize(oarchive); //oarchive(saves); // Save the modified instance.
+		ofs.close();
+		
 	}
 	else if (command.compare("cam_play") == 0)
 	{
